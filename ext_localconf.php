47b89257ebf966c202978d6ec91bc45ccc1e6327
Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com>
2023-08-21 11:59:43 +0200

    feat: add xclass to override teplate ref:ITZBUNDPHP-945

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,14 +7,24 @@
  * LICENSE file that was distributed with this source code.
  */
 
+
+use \ITZBund\A11yBackend\LoginProvider\UsernamePasswordLoginProviderXclass;
+
 defined('TYPO3') or die('Access denied.');
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1529672977] = [
-    'provider' => OAuth2LoginProvider::class,
+/*
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1529357687] = [
+    'provider' => CustomUsernamePasswordLoginProvider::class,
     'sorting' => 25,
+    'iconIdentifier' => 'actions-key',
     'icon-class' => 'fa-sign-in',
-    'label' => 'LLL:EXT:oauth2/Resources/Private/Language/locallang.xlf:oauth2.login.link'
+    'label' => 'fooo'
+];*/
+//todo: replace wen other method is avalible
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\LoginProvider\UsernamePasswordLoginProvider::class] = [
+    'className' => UsernamePasswordLoginProviderXclass::class,
 ];
+
 
 (function () {
     // https://docs.typo3.org/c/typo3/cms-core/main/en-us//Changelog/12.3/Deprecation-100033-TBE_STYLESStylesheetAndStylesheet2.html

0d6a03ba18708282d983c199962cf4858844618f
Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com>
2023-08-21 12:34:06 +0200

    fix: fix linting errors

--- Classes/LoginProvider/UsernamePasswordLoginProviderXclass.php
+++ Classes/LoginProvider/UsernamePasswordLoginProviderXclass.php
@@ -1,12 +1,9 @@
 <?php
 
-
-
 namespace ITZBund\A11yBackend\LoginProvider;
 
 use TYPO3\CMS\Backend\Authentication\PasswordReset;
 use TYPO3\CMS\Backend\Controller\LoginController;
-use TYPO3\CMS\Backend\LoginProvider\LoginProviderInterface;
 use TYPO3\CMS\Backend\LoginProvider\UsernamePasswordLoginProvider;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -21,7 +18,7 @@ class UsernamePasswordLoginProviderXclass extends UsernamePasswordLoginProvider
 {
     public function render(StandaloneView $view, PageRenderer $pageRenderer, LoginController $loginController): void
     {
-       //todo: replace wen other method is avalible
+        //todo: replace wen other method is avalible
         $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:a11y_backend/Resources/Private/Backend/TemplateOverrides/Templates/Login/UserPassLoginForm.html'));
         $request = $loginController->getCurrentRequest();
         if ($request->getAttribute('normalizedParams')->isHttps()) {
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,8 +7,7 @@
  * LICENSE file that was distributed with this source code.
  */
 
-
-use \ITZBund\A11yBackend\LoginProvider\UsernamePasswordLoginProviderXclass;
+use ITZBund\A11yBackend\LoginProvider\UsernamePasswordLoginProviderXclass;
 
 defined('TYPO3') or die('Access denied.');
 
@@ -25,10 +24,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\LoginProvider\Us
     'className' => UsernamePasswordLoginProviderXclass::class,
 ];
 
-
 (function () {
     // https://docs.typo3.org/c/typo3/cms-core/main/en-us//Changelog/12.3/Deprecation-100033-TBE_STYLESStylesheetAndStylesheet2.html
     $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['a11y_backend'] = 'EXT:a11y_backend/Resources/Public/StyleSheets/screen.css';
-
-
 })();

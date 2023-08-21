f757519a46f89d641cd1665514ff0c27dbab73aa
Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com>
2023-08-21 16:52:05 +0200

    feat: xclass login controller

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,21 +7,13 @@
  * LICENSE file that was distributed with this source code.
  */
 
-use ITZBund\A11yBackend\LoginProvider\UsernamePasswordLoginProviderXclass;
 
 defined('TYPO3') or die('Access denied.');
 
-/*
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1529357687] = [
-    'provider' => CustomUsernamePasswordLoginProvider::class,
-    'sorting' => 25,
-    'iconIdentifier' => 'actions-key',
-    'icon-class' => 'fa-sign-in',
-    'label' => 'fooo'
-];*/
+
 //todo: replace wen other method is avalible
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\LoginProvider\UsernamePasswordLoginProvider::class] = [
-    'className' => UsernamePasswordLoginProviderXclass::class,
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\Controller\LoginController::class] = [
+    'className' => \ITZBund\A11yBackend\Controller\LoginControllerXclass::class,
 ];
 
 (function () {

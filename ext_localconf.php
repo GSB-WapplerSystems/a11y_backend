6ec3e7e638d52b30d1fc57fd09b98835ee981b6d
Kai Ole Hartwig <o.hartwig@moselwal.de>
2024-04-10 19:42:34 +0200

    chore: copyright
    Refs: NO-JIRA

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,10 @@
 <?php
 
 /*
- * This file is part of the package itzbund/a11y-backend of the GSB 11 Project by ITZBund
+ * This file is part of the package itzbund/a11y-backend of the GSB 11 Project by ITZBund.
  *
- * (c) Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com> 2023
- * (c) Kai Ole Hartwig <o.hartwig@moselwal.de> 2023
- * (c) Michael Max Busch <m.busch@moselwal.de> 2023
+ * Copyright (C) 2023 - 2024 Bundesrepublik Deutschland, vertreten durch das
+ * BMI/ITZBund. Author: Michael Max Busch, Ole Hartwig, Christian Rath-Ulrich
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2

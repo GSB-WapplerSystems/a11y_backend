5647fa83b29f81c001bbd2d955451082f688c876
Christian Rath-Ulrich <christian.rath-ulrich@digitas.com>
2024-05-07 17:42:22 +0200

    feat: Add visibility toggle to sys_file_metadata and provide translations
    Implemented a new 'visible' column to the 'sys_file_metadata' table along with its configuration. In addition, translations for the newly added field have been created in English and German for better localization.
    
    Refs: ITZBUNDPHP-2231

---
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+defined('TYPO3') || die();
+
+(static function (): void {
+    $newColumns = [
+        'visible' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:a11y_backend/Resources/Private/Language/locallang_db.xlf:sys_file_metadata.visible.label',
+            'description' => 'LLL:EXT:a11y_backend/Resources/Private/Language/locallang_db.xlf:sys_file_metadata.visible.description',
+            'config' => [
+                'renderType' => 'checkboxToggle',
+                'type' => 'check',
+                'placeholder' => '__row|uid_local|metadata|visible',
+                'mode' => 'useOrOverridePlaceholder',
+                'default' => null,
+            ],
+        ],
+    ];
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_metadata', $newColumns);
+})();
